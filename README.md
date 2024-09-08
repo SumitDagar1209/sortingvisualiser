e638aade29953345691b987e90dbca1fe53e53c1
sumit dagar <sumitdagar@sumits-MacBook-Air.local>
2024-09-08 14:01:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![Screenshot (578)](https://github.com/NikanshuGoyal/sorting-visualiser/assets/102424293/48a9350f-018a-4b42-af6b-218eebf0b97d)
+![Screenshot (577)](https://github.com/NikanshuGoyal/sorting-visualiser/assets/102424293/62e58841-1989-4f0b-9cda-fd7489f0dd8e)
+![Screenshot (579)](https://github.com/NikanshuGoyal/sorting-visualiser/assets/102424293/d7b3f7fb-c73b-4cae-8603-bebf2d66bc6a)
---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        Designed and built with{' '}
+        <span className="Footer__Heart">&hearts;</span> by{' '}
+        <a
+        
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Sumit Dagar
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+       
+       
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
